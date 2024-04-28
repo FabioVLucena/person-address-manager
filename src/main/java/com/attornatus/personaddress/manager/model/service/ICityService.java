e5ec32cb54edc070d480d05e72fc75fd056cbe01
aeon <fabio-trab@outlook.com>
2024-04-28 14:38:17 -0400

    feat: add city service and repository

---
+++ src/main/java/com/attornatus/personaddress/manager/model/service/ICityService.java
@@ -0,0 +1,14 @@
+package com.attornatus.personaddress.manager.model.service;
+
+import java.util.List;
+
+import com.attornatus.personaddress.manager.exception.NotFoundException;
+import com.attornatus.personaddress.manager.model.entity.City;
+
+public interface ICityService {
+
+	City getCityById(Long cityId) throws NotFoundException;
+	
+	List<City> findAllCitys();
+	
+}
---
+++ src/main/java/com/attornatus/personaddress/manager/service/CityService.java
@@ -0,0 +1,33 @@
+package com.attornatus.personaddress.manager.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.attornatus.personaddress.manager.exception.NotFoundException;
+import com.attornatus.personaddress.manager.model.entity.City;
+import com.attornatus.personaddress.manager.model.service.ICityService;
+import com.attornatus.personaddress.manager.repository.CityRepository;
+
+@Service
+public class CityService implements ICityService {
+
+	private CityRepository cityRepository;
+	
+	@Autowired
+	public CityService(CityRepository cityRepository) {
+		this.cityRepository = cityRepository;
+	}
+	
+	@Override
+	public City getCityById(Long cityId) throws NotFoundException {
+		return this.cityRepository.findById(cityId)
+				.orElseThrow(() -> new NotFoundException("City with id: " + cityId + " not found!"));
+	}
+
+	@Override
+	public List<City> findAllCitys() {
+		return this.cityRepository.findAll();
+	}
+}
