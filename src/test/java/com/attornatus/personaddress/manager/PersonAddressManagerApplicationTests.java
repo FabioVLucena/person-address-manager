9e2f0c32694c2f156206693326c331d5be2991be
aeon <fabio-trab@outlook.com>
2024-04-27 23:27:48 -0400

    feat: first commit

---
+++ src/test/java/com/attornatus/personaddress/manager/PersonAddressManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatus.personaddress.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonAddressManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
